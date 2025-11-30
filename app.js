a1ea26e50e1c6ef9070fd24c615ac521f2b6d9a0
Ronak A. Sheladiya <126866075+Ronak-Sheladiya@users.noreply.github.com>
2025-12-01 01:51:38 +0530

    Minimal working setup

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello World - PixelVault is running!');
+  res.send('Hello World!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
