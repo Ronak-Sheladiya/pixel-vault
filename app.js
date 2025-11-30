28449724365a785bbd012314fe81869fdd3b1a86
Ronak A. Sheladiya <126866075+Ronak-Sheladiya@users.noreply.github.com>
2025-12-01 02:22:10 +0530

    Absolute minimal Node.js app

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
 
-app.get('/', function(req, res) {
-    res.send('Hello World!');
+app.get('/', (req, res) => {
+  res.send('Hello World');
 });
 
-app.listen(port, function() {
-    console.log('Example app listening on port ' + port);
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
 });
